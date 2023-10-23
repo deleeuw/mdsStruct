f83746623693c76e1c700ac85da673a63a046cae
Jan de Leeuw <jan@deleeuwpdx.net>
2023-10-23 19:37:20 -0400

    hessian

--- ccode/smacofDerivatives.c
+++ ccode/smacofDerivatives.c
@@ -37,8 +37,8 @@ void smacofGradient(const double *delta, const double *weights,
 
 void smacofHessian(const double *delta, const double *weights,
                    const double *xconf, const double *dmat, const double *bmat,
-                   const double *v) {
-    int n = 10, p = 2, m = n * (n - 1) / 2, q = p * (p - 1) / 2;
+                   const double *v, const int *pn, const int *pp) {
+    int n = *pn, p = *pp, m = n * (n - 1) / 2, q = p * (p - 1) / 2;
     double **diagH = (double **)calloc((size_t)p, (size_t)sizeof(double *));
     double **nonDiagH = (double **)calloc((size_t)q, (size_t)sizeof(double *));
     for (int s = 1; s <= p; s++) {
@@ -50,25 +50,48 @@ void smacofHessian(const double *delta, const double *weights,
             int st = SINDEX(s, t, p);
             nonDiagH[st] =
                 (double *)calloc((size_t)(n * n), (size_t)sizeof(double));
-            for (int j = 1; j <= (n - 1); j++) {
-                int js = MINDEX(j, s, n);
-                int jt = MINDEX(j, t, n);
-                for (int i = (j + 1); i <= n; i++) {
-                    int ij = SINDEX(i, j, n);
-                    int is = MINDEX(i, s, n);
-                    int it = MINDEX(i, t, n);
+            for (int i = 1; i <= n; i++) {
+                int is = MINDEX(i, s, n);
+                int it = MINDEX(i, t, n);
+                for (int j = 1; j <= n; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+                    int ij = PINDEX(i, j, n);
+                    int js = MINDEX(j, s, n);
+                    int jt = MINDEX(j, t, n);
                     double f = weights[ij] * delta[ij] / THIRD(dmat[ij]);
-                    nonDiagH[st][ij] = f * (xconf[is] - xconf[js]) * (xconf[it] - xconf[jt]);
+                    nonDiagH[st][ij] =
+                        f * (xconf[is] - xconf[js]) * (xconf[it] - xconf[jt]);
                 }
             }
         }
     }
-    for (int i = 1; i <= p; i++) {
-        // make diagH[VINDEX(i)]
+    // make diagH[VINDEX(i)]
+    for (int s = 1; s <= p; s++) {
+        int ss = VINDEX(s);
+        diagH[VINDEX(s)] = (double *)calloc((size_t)m, (size_t)sizeof(double));
+        for (int j = 1; j <= (n - 1); j++) {
+            int js = MINDEX(j, s, n);
+            for (int i = (j + 1); i <= n; i++) {
+                int ij = SINDEX(i, j, n);
+                int is = MINDEX(i, s, n);
+                double f = weights[ij] * delta[ij] / THIRD(dmat[ij]);
+                diagH[ss][ij] = f * SQUARE(xconf[is] - xconf[js]);
+                diagH[ss][ij] += (v[ij] - bmat[ij]);
+            }
+        }
     }
     for (int s = 1; s <= p; s++) {
         free(diagH[VINDEX(s)]);
     }
     free(diagH);
+    for (int t = 1; t <= (p - 1); t++) {
+      for (int s = (t + 1); s <= p; s++) {
+        int st = SINDEX(s, t, p);
+        free(nonDiagH[st]);
+      }
+    }
+    free(nonDiagH);
     return;
 }
